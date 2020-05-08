3410f91e4db09a5aa781120acbacb97a5ace6dba
hwchan <honwen.chan@gmail.com>
2020-05-08 19:52:49 +0800

    adjust build.sh

--- build-release.sh
+++ build-release.sh
@@ -13,8 +13,7 @@ fi
 # fi
 
 VERSION=$(curl -sSL https://api.github.com/repos/chenhw2/aliyun-ddns-cli/commits/master | sed -n '{/sha/p; /date/p;}'| sed 's/.* \"//g' | cut -c1-10 | tr '[:lower:]' '[:upper:]' | sed 'N;s/\n/@/g' | head -1)
-LDFLAGS="-X main.version=$VERSION -s -w -linkmode external -extldflags -static"
-GCFLAGS=""
+LDFLAGS="-X main.version=$VERSION -s -w"
 
 # X86
 OSES=(windows linux darwin freebsd)
@@ -26,32 +25,31 @@ for os in ${OSES[@]}; do
 		suffix=""
 		if [ "$os" == "windows" ]; then
 			suffix=".exe"
-			LDFLAGS="-X main.version=$VERSION -s -w"
 		fi
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_${os}_${arch}${suffix} .
+		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -o ./release/${name}_${os}_${arch}${suffix} .
 		# if $UPX; then upx -9 ./release/${name}_${os}_${arch}${suffix} -o ./release/${name}_${os}_${arch}${suffix}_upx; fi
 		tar -C ./release -zcf ./release/${name}_${os}-${arch}-$VERSION.tar.gz ./${name}_${os}_${arch}${suffix}
 		$MD5 ./release/${name}_${os}-${arch}-$VERSION.tar.gz
 	done
 done
 
+# ARM64
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o ./release/${name}_arm64 .
 # ARM
 ARMS=(5 6 7)
 for v in ${ARMS[@]}; do
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_arm$v .
+	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -o ./release/${name}_arm$v .
 done
 # if $UPX; then upx -9 ./release/${name}_arm*; fi
 tar -C ./release -zcf ./release/${name}_arm-$VERSION.tar.gz $(for v in ${ARMS[@]}; do echo -n "./${name}_arm$v ";done)
 $MD5 ./release/${name}_arm-$VERSION.tar.gz
 
-# MIPS # go 1.8+ required
-LDFLAGS="-X main.version=$VERSION -s -w"
-env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_mipsle .
-env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_mips .
-# MIPS # go 1.10+ required
-env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_mipsle_sf .
-env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=softfloat go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ./release/${name}_mips_sf .
-
+# MIPS/hardfloat
+env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "$LDFLAGS" -o ./release/${name}_mipsle .
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "$LDFLAGS" -o ./release/${name}_mips .
+# MIPS/softfloat
+env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -ldflags "$LDFLAGS" -o ./release/${name}_mipsle_sf .
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=softfloat go build -ldflags "$LDFLAGS" -o ./release/${name}_mips_sf .
 # if $UPX; then upx -9 ./release/${name}_mips**; fi
 tar -C ./release -zcf ./release/${name}_mipsle-$VERSION.tar.gz ./${name}_mipsle
 tar -C ./release -zcf ./release/${name}_mips-$VERSION.tar.gz ./${name}_mips
