4a47e2a91888f1d944680e8f6662d85dfe15492b
honwen.chan <honwen.chan@gmail.com>
2021-04-23 00:44:30 +0800

    replace dnsproxy with doggo

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/honwen/aliyun-ddns-cli
 go 1.16
 
 require (
-	github.com/AdguardTeam/dnsproxy v0.37.1
-	github.com/AdguardTeam/golibs v0.4.4
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/denverdino/aliyungo v0.0.0-20210318042315-546d0768f5c7
-	github.com/honwen/ip2loc v0.1.0
+	github.com/honwen/ip2loc v0.1.1
 	github.com/honwen/tldextract v0.1.0
 	github.com/miekg/dns v1.1.41
+	github.com/mr-karan/doggo v0.3.9
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli v1.22.5
 )
--- utils.go
+++ utils.go
@@ -10,11 +10,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/AdguardTeam/dnsproxy/upstream"
-	"github.com/AdguardTeam/golibs/utils"
+	"github.com/asaskevich/govalidator"
 	"github.com/honwen/ip2loc"
 	"github.com/honwen/tldextract"
 	"github.com/miekg/dns"
+	"github.com/mr-karan/doggo/pkg/resolvers"
+	"github.com/mr-karan/doggo/pkg/utils"
 )
 
 const minTimeout = 2000 * time.Millisecond
@@ -32,16 +33,41 @@ var ip6API = []string{
 	"http://speed.neu6.edu.cn/getIP.php", "http://v6.myip.la", "http://api-ipv6.ip.sb/ip", "http://ip6only.me/api/", "http://v6.ipv6-test.com/api/myip.php", "https://v6.ident.me",
 }
 
-var dnsUpStream = []string{
-	"tls://223.5.5.5", "tls://223.6.6.6", "https://223.5.5.5/dns-query", "https://223.6.6.6/dns-query", "https://dns.alidns.com/dns-query",
-}
-
 var curlVer = []string{
 	"7.75.0", "7.74.0", "7.73.0", "7.72.0", "7.71.1", "7.71.0", "7.70.0", "7.69.1", "7.69.0", "7.68.0", "7.67.0", "7.66.0",
 	"7.65.3", "7.65.2", "7.65.1", "7.65.0", "7.64.1", "7.64.0", "7.63.0", "7.62.0", "7.61.1", "7.61.0", "7.60.0", "7.59.0",
 	"7.58.0", "7.57.0", "7.56.1", "7.56.0", "7.55.1", "7.55.0", "7.54.1", "7.54.0", "7.53.1", "7.53.0", "7.52.1", "7.52.0",
 	"7.51.0", "7.50.3", "7.50.2", "7.50.1", "7.50.0", "7.49.1", "7.49.0", "7.48.0", "7.47.1", "7.47.0", "7.46.0", "7.45.0",
 	"7.44.0", "7.43.0", "7.42.1", "7.42.0", "7.41.0", "7.40.0", "7.39.0", "7.38.0", "7.37.1", "7.37.0", "7.36.0",
+}
+
+var dnsUpStream = []resolvers.Resolver{}
+
+func init() {
+	var upStreams = []string{
+		"tls://223.5.5.5:853", "tls://223.6.6.6:853", "https://223.5.5.5/dns-query", "https://223.6.6.6/dns-query",
+	}
+	var opts = resolvers.Options{
+		Timeout: 2000 * time.Millisecond,
+		Logger:  utils.InitLogger(),
+	}
+	var dotOpts = resolvers.ClassicResolverOpts{
+		UseTLS: true,
+		UseTCP: true,
+	}
+
+	for _, upstream := range upStreams {
+		var ns resolvers.Resolver
+		switch {
+		case strings.HasPrefix(upstream, "https://"):
+			ns, _ = resolvers.NewDOHResolver(upstream, opts)
+		case strings.HasPrefix(upstream, "tls://"):
+			ns, _ = resolvers.NewClassicResolver(upstream[6:], dotOpts, opts)
+		default:
+			continue
+		}
+		dnsUpStream = append(dnsUpStream, ns)
+	}
 }
 
 func getIP() (ip string) {
@@ -113,18 +139,21 @@ func getDNS(domain string, ipv6 bool) (ip string) {
 		dnsMap   = make(map[string]int, len(dnsUpStream))
 		cchan    = make(chan string, len(dnsUpStream))
 		maxCount = -1
-		timeout  = 2000 * time.Millisecond
 	)
 
-	for i := 0; i < len(dnsUpStream); i++ {
-		go func(dns string) {
-			resolver, _ := upstream.AddressToUpstream(dns, upstream.Options{Timeout: timeout})
+	for i := range dnsUpStream {
+		go func(resolver *resolvers.Resolver) {
+			qtype := dns.TypeA
 			if ipv6 {
-				cchan <- getFisrtAAAARecord(resolver, dns, domain)
-			} else {
-				cchan <- getFisrtARecord(resolver, dns, domain)
+				qtype = dns.TypeAAAA
 			}
-		}(dnsUpStream[i])
+			resp, err := (*resolver).Lookup(dns.Question{Name: domain, Qtype: qtype})
+			if err == nil && len(resp.Answers) > 0 {
+				cchan <- resp.Answers[0].Address
+			} else {
+				cchan <- "" // SOA
+			}
+		}(&dnsUpStream[i])
 	}
 
 	for i := 0; i < len(dnsUpStream); i++ {
@@ -142,46 +171,6 @@ func getDNS(domain string, ipv6 bool) (ip string) {
 		if v > maxCount {
 			maxCount = v
 			ip = k
-		}
-	}
-	return
-}
-
-func getFisrtARecord(resolver upstream.Upstream, dnsServer, targetDomain string) (ip string) {
-	if !strings.HasSuffix(targetDomain, ".") {
-		targetDomain += "."
-	}
-	msg := new(dns.Msg)
-	msg.SetQuestion(targetDomain, dns.TypeA)
-	r, err := resolver.Exchange(msg)
-	if err != nil && (r == nil || r.Rcode != dns.RcodeSuccess) {
-		log.Printf("%+v", err)
-		return
-	}
-	for _, rr := range r.Answer {
-		if a, ok := rr.(*dns.A); ok {
-			ip = a.A.String()
-			break
-		}
-	}
-	return
-}
-
-func getFisrtAAAARecord(resolver upstream.Upstream, dnsServer, targetDomain string) (ip string) {
-	if !strings.HasSuffix(targetDomain, ".") {
-		targetDomain += "."
-	}
-	msg := new(dns.Msg)
-	msg.SetQuestion(targetDomain, dns.TypeAAAA)
-	r, err := resolver.Exchange(msg)
-	if err != nil && (r == nil || r.Rcode != dns.RcodeSuccess) {
-		log.Printf("%+v", err)
-		return
-	}
-	for _, rr := range r.Answer {
-		if a, ok := rr.(*dns.AAAA); ok {
-			ip = a.AAAA.String()
-			break
 		}
 	}
 	return
@@ -214,7 +203,7 @@ func splitDomain(fulldomain string) (rr, domain string) {
 	}
 
 	domainInfo := tldextract.New().Extract(fulldomain)
-	if utils.IsValidHostname(fulldomain+`.`) == nil || len(domainInfo.Tld) == 0 || len(domainInfo.Root) == 0 {
+	if !govalidator.IsDNSName(fulldomain) || len(domainInfo.Tld) == 0 || len(domainInfo.Root) == 0 {
 		log.Fatal("Not a Vaild Domain")
 		return
 	}
